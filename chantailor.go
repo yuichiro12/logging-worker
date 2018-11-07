34cea039224d3e35d123612a1683e8e21f5e57a5
yuichiro12 <talcnrhyme@gmail.com>
2018-11-07 15:57:07 +0900

    add loggers

---
+++ chantailor.go
@@ -0,0 +1,35 @@
+package chantailor
+
+import (
+	"io"
+	"log"
+	"strings"
+)
+
+type Logger struct {
+	ColumnSeparator string
+	RowSeparator    string
+}
+
+func NewLogger(csep, rsep string) Logger {
+	return Logger{
+		ColumnSeparator: csep,
+		RowSeparator:    rsep,
+	}
+}
+
+func (l Logger) LogRow(w io.Writer, rc <-chan []string, ec chan<- error) {
+	for r := range rc {
+		if _, err := w.Write([]byte(strings.Join(r, l.ColumnSeparator) + l.RowSeparator)); err != nil {
+			ec <- err
+		}
+	}
+}
+
+func LogError(w io.Writer, ec <-chan error) {
+	for e := range ec {
+		if _, err := w.Write([]byte(e.Error() + "\n")); err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
